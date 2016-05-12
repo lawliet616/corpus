f9541035b4e89f9b07bdc37a7e1023e762e8c106
IronFist166 <kozandris@gmail.com>
2016-05-12 13:50:24 +0200

    Role and RoleConverter added to the project

---
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/util/Role.java
@@ -0,0 +1,7 @@
+package hu.bugbusters.corpus.core.util;
+
+public enum Role {
+	ADMIN,
+	TEACHER,
+	USER
+}
---
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/util/RoleConverter.java
@@ -0,0 +1,35 @@
+package hu.bugbusters.corpus.core.util;
+
+import javax.persistence.AttributeConverter;
+
+public class RoleConverter implements AttributeConverter<Role, Integer>{
+
+	@Override
+	public Integer convertToDatabaseColumn(Role role) {
+		switch(role) {
+		case ADMIN:
+			return new Integer(2);
+		case TEACHER:
+			return new Integer(1);
+		case USER:
+			return new Integer(0);
+		default:
+			throw new IllegalArgumentException("Unknown role: " + role);		
+		}
+	}
+
+	@Override
+	public Role convertToEntityAttribute(Integer data) {
+		switch(data) {
+			case 2:
+				return Role.ADMIN;
+			case 1:
+				return Role.TEACHER;
+			case 0:
+				return Role.USER;
+			default:
+				throw new IllegalArgumentException("Unknown role from database:" + data);
+		}
+	}
+
+}

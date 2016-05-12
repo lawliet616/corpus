d25331a98fcfaea71d3a1755a1d4c240d1abab35
IronFist166 <kozandris@gmail.com>
2016-05-12 14:09:24 +0200

    CorpusUI cleaned from unnecessary lines

--- corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/CorpusUI.java
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/CorpusUI.java
@@ -2,23 +2,14 @@ package hu.bugbusters.corpus.core.vaadin;
 
 import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinService;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 
-import hu.bugbusters.corpus.core.bean.RegisteredUser;
-import hu.bugbusters.corpus.core.dao.Dao;
-import hu.bugbusters.corpus.core.dao.impl.DaoImpl;
 import hu.bugbusters.corpus.core.util.Login;
 import hu.bugbusters.corpus.core.util.Role;
 import hu.bugbusters.corpus.core.vaadin.view.AdminView;
 import hu.bugbusters.corpus.core.vaadin.view.LoginView;
 import hu.bugbusters.corpus.core.vaadin.view.TeacherView;
 import hu.bugbusters.corpus.core.vaadin.view.UserView;
-
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
 
 public class CorpusUI extends UI {
 	Navigator navigator;
@@ -51,8 +42,7 @@ public class CorpusUI extends UI {
 			navigator.navigateTo(UserView.NAME);
 			break;
 		default:
-			navigator.navigateTo(ErrorView.NAME);
-			break;
+			throw new IllegalArgumentException("Unknown view");
 		}
 	}
 }

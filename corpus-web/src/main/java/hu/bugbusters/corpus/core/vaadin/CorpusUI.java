90f267fd1bbb0ccf714cccc68d84b0834915be87
IronFist166 <kozandris@gmail.com>
2016-05-12 14:07:29 +0200

    Role-dependent navigation implemented

--- corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/CorpusUI.java
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/CorpusUI.java
@@ -10,6 +10,8 @@ import com.vaadin.ui.UI;
 import hu.bugbusters.corpus.core.bean.RegisteredUser;
 import hu.bugbusters.corpus.core.dao.Dao;
 import hu.bugbusters.corpus.core.dao.impl.DaoImpl;
+import hu.bugbusters.corpus.core.util.Login;
+import hu.bugbusters.corpus.core.util.Role;
 import hu.bugbusters.corpus.core.vaadin.view.AdminView;
 import hu.bugbusters.corpus.core.vaadin.view.LoginView;
 import hu.bugbusters.corpus.core.vaadin.view.TeacherView;
@@ -19,16 +21,38 @@ import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 
 public class CorpusUI extends UI {
+	Navigator navigator;
 	
 	@Override
 	protected void init(final VaadinRequest request) {
-		Navigator navigator = new Navigator(this, this);
+		navigator = new Navigator(this, this);
 
 		navigator.addView(LoginView.NAME, new LoginView());
 		navigator.addView(UserView.NAME, new UserView());
 		navigator.addView(TeacherView.NAME, new TeacherView());
 		navigator.addView(AdminView.NAME, new AdminView());
 		
-		navigator.navigateTo(LoginView.NAME);
+		if(!Login.loggedIn()) {
+			navigator.navigateTo(LoginView.NAME);
+		} else {
+			Login.navigateByRole();
+		}
+	}
+
+	public void navigateToViewByRole(Role role) {
+		switch(role) {
+		case ADMIN:
+			navigator.navigateTo(AdminView.NAME);
+			break;
+		case TEACHER:
+			navigator.navigateTo(TeacherView.NAME);
+			break;
+		case USER:
+			navigator.navigateTo(UserView.NAME);
+			break;
+		default:
+			navigator.navigateTo(ErrorView.NAME);
+			break;
+		}
 	}
 }

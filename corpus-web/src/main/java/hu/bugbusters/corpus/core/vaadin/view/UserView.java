3125f8cc581538ca6ee376ed705c93b29892af47
IronFist166 <kozandris@gmail.com>
2016-05-12 11:58:52 +0200

    Initial commit

---
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/view/UserView.java
@@ -0,0 +1,15 @@
+package hu.bugbusters.corpus.core.vaadin.view;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.CustomComponent;
+
+public class UserView extends CustomComponent implements View {
+	public static final String NAME = "user";
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+}

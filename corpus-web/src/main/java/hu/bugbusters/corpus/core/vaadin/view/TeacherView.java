ba21663a8a11816b4a0ad235293a511181a640eb
IronFist166 <kozandris@gmail.com>
2016-05-12 12:00:59 +0200

    Initial commit

---
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/view/TeacherView.java
@@ -0,0 +1,15 @@
+package hu.bugbusters.corpus.core.vaadin.view;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.CustomComponent;
+
+public class TeacherView extends CustomComponent implements View {
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

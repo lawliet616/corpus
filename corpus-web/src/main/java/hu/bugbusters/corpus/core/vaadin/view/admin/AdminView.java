25a92caa0a0429b7ae86db0156bdfee1e926598a
ZalaváriBálint <zalavarib2121@gmail.com>
2016-05-28 21:19:49 +0200

    Remove unused imports

--- corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/view/admin/AdminView.java
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/view/admin/AdminView.java
@@ -1,11 +1,6 @@
 package hu.bugbusters.corpus.core.vaadin.view.admin;
 
 import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.Window;
-
-import hu.bugbusters.corpus.core.dao.Dao;
 import hu.bugbusters.corpus.core.dao.impl.DaoImpl;
 import hu.bugbusters.corpus.core.login.Role;
 import hu.bugbusters.corpus.core.vaadin.view.CorpusView;
@@ -16,10 +11,8 @@ public class AdminView extends AdminDesign implements CorpusView{
 	public static final String NAME = "Admin";
 	public static final Role ROLE   = Role.ADMIN;
 
-	private Dao dao;
-
 	public AdminView(){
-		dao = new DaoImpl();
+		new DaoImpl();
 		logoutButton.addClickListener(new LogoutClickListener());
 	}
 

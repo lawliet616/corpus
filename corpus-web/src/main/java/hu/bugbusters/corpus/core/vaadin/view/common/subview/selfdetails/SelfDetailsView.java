7e01fb85a834bc1246bdb0a905081cc86fee3021
ZalaváriBálint <zalavarib2121@gmail.com>
2016-05-28 23:17:29 +0200

    fix the fillTable method

--- corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/view/common/subview/selfdetails/SelfDetailsView.java
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/view/common/subview/selfdetails/SelfDetailsView.java
@@ -4,6 +4,7 @@ package hu.bugbusters.corpus.core.vaadin.view.common.subview.selfdetails;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 
+import hu.bugbusters.corpus.core.bean.RegisteredUser;
 import hu.bugbusters.corpus.core.login.Login;
 
 @SuppressWarnings("serial")
@@ -20,9 +21,10 @@ public class SelfDetailsView extends SelfDetailsDesign implements View{
 	}
 
 	private void fillTable() {
-		lblEmail.setValue(Login.getLoggedInUser().getEmail());
-		lblName.setValue(Login.getLoggedInUser().getFullname());
-		lblUsername.setValue(Login.getLoggedInUser().getUsername());
+		RegisteredUser user = Login.getLoggedInUser();
+		lblEmail.setValue(user.getEmail());
+		lblName.setValue(user.getFullname());
+		lblUsername.setValue(user.getUsername());
 		
 	}
 

5ec8520988107827c044c3d75cdd57bda6703c7f
ZalaváriBálint <zalavarib2121@gmail.com>
2016-05-29 14:41:17 +0200

    add passwordChangeView

---
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/view/common/subview/selfdetails/ChangePasswordView.java
@@ -0,0 +1,13 @@
+package hu.bugbusters.corpus.core.vaadin.view.common.subview.selfdetails;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+
+public class ChangePasswordView extends ChangePasswordDesign implements View {
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+				
+	}
+
+}

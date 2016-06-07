3c9ef053016d80fbb3c54063cb8467184c579b18
Ahim13 <ahim13@gmail.com>
2016-06-07 20:33:27 +0200

    Added newMail window

---
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/view/common/email/emailsubviews/newMailView.java
@@ -0,0 +1,13 @@
+package hu.bugbusters.corpus.core.vaadin.view.common.email.emailsubviews;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+
+@SuppressWarnings("serial")
+public class newMailView extends newMailDesign implements View {
+
+	@Override
+	public void enter(ViewChangeEvent event) {	
+	}
+
+}

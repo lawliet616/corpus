e7c31cadf303b4c05496ab158a44a4a44190c221
Valentin Krasznai <valentin.krasznai@gmail.com>
2016-06-07 22:37:21 +0200

    Added proper message preview

---
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/util/StringUtils.java
@@ -0,0 +1,22 @@
+package hu.bugbusters.corpus.core.util;
+
+public class StringUtils {
+    private static final int PREVIEW_MAX_LENGTH = 50;
+
+    public static String createPreviewFromMessage(String message){
+        if(message.length() < PREVIEW_MAX_LENGTH){
+            return message;
+        } else{
+            String preview = message.substring(0, PREVIEW_MAX_LENGTH);
+
+            while(preview.endsWith(" ")){
+                preview = preview.substring(0, preview.length() - 1);
+            }
+
+            preview = preview + "...";
+
+            return preview;
+        }
+    }
+}
+
--- corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/view/common/email/emailsubviews/emailTextView.java
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/view/common/email/emailsubviews/emailTextView.java
@@ -1,5 +1,6 @@
 package hu.bugbusters.corpus.core.vaadin.view.common.email.emailsubviews;
 
+import java.util.Objects;
 import java.util.Set;
 
 import com.vaadin.navigator.View;
@@ -11,45 +12,54 @@ import hu.bugbusters.corpus.core.bean.Inbox;
 import hu.bugbusters.corpus.core.bean.Message;
 import hu.bugbusters.corpus.core.dao.Dao;
 import hu.bugbusters.corpus.core.dao.impl.DaoImpl;
+import hu.bugbusters.corpus.core.exceptions.UserNotFoundException;
 import hu.bugbusters.corpus.core.login.Login;
+import hu.bugbusters.corpus.core.util.StringUtils;
 
 @SuppressWarnings("serial")
 public class emailTextView extends emailTextDesign implements View {
-	
+	private static final String STYLE_SEEN = "seen";
+	private static final String STYLE_NOT_SEEN = "notSeen";
 	private Dao dao = DaoImpl.getInstance();
 
 	public emailTextView(final Message message, char seen) {
-		if(seen == 'N'){
-			seenButton.setStyleName("notSeen");
-		}else{
-			seenButton.setStyleName("seen");
+		if (seen == 'N') {
+			seenButton.setStyleName(STYLE_NOT_SEEN);
+		} else {
+			seenButton.setStyleName(STYLE_SEEN);
 			seenButton.setEnabled(false);
 		}
-		
-		senderNameLabel.setValue("Anyánk");
+
+		try {
+			senderNameLabel.setValue(dao.getUserById(message.getCreatorId()).getFullName());
+		} catch (UserNotFoundException e) {
+			senderNameLabel.setValue("NOT_FOUND");
+		}
+        
 		messageLabel.setCaption(message.getSubject());
-		messageLabel.setValue(message.getMessage());
-		
+		messageLabel.setValue(StringUtils.createPreviewFromMessage(message.getMessage()));
+
+
 		seenButton.addClickListener(new ClickListener() {
-			
+
 			@Override
 			public void buttonClick(ClickEvent event) {
-				seenButton.setStyleName("seen");
+				seenButton.setStyleName(STYLE_SEEN);
 				seenButton.setEnabled(false);
-				
+
 				Set<Inbox> receivedMessages = Login.getLoggedInUser().getReceivedMails();
-				
-				for (Inbox receivedMessage : receivedMessages){
-					if(receivedMessage.getMessage().getId() == message.getId()){
+
+				for (Inbox receivedMessage : receivedMessages) {
+					if (Objects.equals(receivedMessage.getMessage().getId(), message.getId())) {
 						receivedMessage.setSeen('Y');
 					}
 				}
-				
+
 				dao.updateEntity(receivedMessages);
 			}
 		});
 	}
-	
+
 	@Override
 	public void enter(ViewChangeEvent event) {
 	}

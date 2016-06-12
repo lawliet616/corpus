5e2c557ca27b55556376390877d734acbb0fca63
ZalaváriBálint <zalavarib2121@gmail.com>
2016-06-12 23:38:16 +0200

    add function to btnSave on adminCourseList

--- corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/view/admin/courselist/AdminCourseListView.java
+++ corpus-web/src/main/java/hu/bugbusters/corpus/core/vaadin/view/admin/courselist/AdminCourseListView.java
@@ -1,14 +1,12 @@
 package hu.bugbusters.corpus.core.vaadin.view.admin.courselist;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.security.auth.callback.ConfirmationCallback;
-
+import com.vaadin.data.Validator;
+import com.vaadin.data.Validator.InvalidValueException;
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
 import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
 import com.vaadin.data.util.BeanContainer;
 import com.vaadin.data.util.BeanItem;
+import com.vaadin.data.validator.IntegerRangeValidator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.shared.ui.MarginInfo;
@@ -20,11 +18,10 @@ import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Table;
-import com.vaadin.ui.TextArea;
 import com.vaadin.ui.Table.ColumnGenerator;
+import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
@@ -33,15 +30,25 @@ import hu.bugbusters.corpus.core.bean.Course;
 import hu.bugbusters.corpus.core.bean.RegisteredUser;
 import hu.bugbusters.corpus.core.dao.Dao;
 import hu.bugbusters.corpus.core.dao.impl.DaoImpl;
-import hu.bugbusters.corpus.core.exceptions.CourseNotFoundException;
+import hu.bugbusters.corpus.core.exceptions.UserNotFoundException;
+import hu.bugbusters.corpus.core.factories.CourseFactory;
+import hu.bugbusters.corpus.core.global.Global;
 import hu.bugbusters.corpus.core.login.Role;
 
 public class AdminCourseListView extends AdminCourseListDesign implements View {
 	private static final long serialVersionUID = -2216242736099627777L;
 	public final static String NAME = "AdminCourseList";
-	BeanContainer<Long, Course> courseDataSource;
+	private BeanContainer<Long, Course> courseDataSource;
+	private BeanFieldGroup<Course> courseBinder;
 	protected Window editCourseWindow;
 	private Dao dao;
+	private TextField idField;
+	private TextField nameField;
+	private TextField creditField;
+	private TextField roomField;
+	private TextField sizeField;
+	private TextArea descriptionArea;
+	private ComboBox teacherField;
 
 	public AdminCourseListView() {
 		dao = DaoImpl.getInstance();
@@ -92,7 +99,7 @@ public class AdminCourseListView extends AdminCourseListDesign implements View {
 
 	private Component createCourseEditLayout(Course course) {
 		VerticalLayout verticalLayout = new VerticalLayout();
-		final BeanFieldGroup<Course> courseBinder = new BeanFieldGroup<Course>(Course.class);
+		courseBinder = new BeanFieldGroup<Course>(Course.class);
 
 		courseBinder.setItemDataSource(course);
 		verticalLayout.addComponent(createCourseEditForm(courseBinder));
@@ -103,7 +110,7 @@ public class AdminCourseListView extends AdminCourseListDesign implements View {
 		Button btnSave = new Button("Mentés");
 		Button btnDelete = new Button("Törlés");
 
-		addListenerToButtons(courseBinder, btnSave, btnDelete);
+		addListenerToButtons(btnSave, btnDelete);
 
 		buttonLayout.addComponent(btnSave);
 		buttonLayout.addComponent(btnDelete);
@@ -113,55 +120,79 @@ public class AdminCourseListView extends AdminCourseListDesign implements View {
 		return verticalLayout;
 	}
 
-	private void addListenerToButtons(BeanFieldGroup<Course> courseBinder, Button btnSave, Button btnDelete) {
-		try {
-			courseBinder.commit();
-			Course bean = courseBinder.getItemDataSource().getBean();
-			if (bean.getName() == null || bean.getRoom() == null || bean.getTeacher() == null) {
-				throw new Exception("A mező értékek nem felelnek meg.");
-			}
-		} catch (CommitException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
+	private void addListenerToButtons(Button btnSave, Button btnDelete) {
+		Course bean = courseBinder.getItemDataSource().getBean();
+		btnSave.addClickListener(new ClickListener() {
 
+			@Override
+			public void buttonClick(ClickEvent event) {
+				saveCourse();
+			}
+		});
+	}
+
+	protected void saveCourse() {
+		try {
+			Course course = courseBinder.getItemDataSource().getBean();
+			course.setTeacher(teacherField.getValue().toString());
+			courseBinder.commit();
+
+			dao.updateEntity(course);
+			RegisteredUser teacher = dao.getUserByFullName(teacherField.getValue().toString());
+			CourseFactory.registerForCourse(teacher, course);
+			editCourseWindow.close();
+			courseTable.refreshRowCache();
+			Notification.show("Sikeresen elmentette a változásokat!", Notification.Type.HUMANIZED_MESSAGE);
+		} catch (CommitException e) {
+			Notification.show("Nem megfelő formátumú értéket adott meg.", Notification.Type.WARNING_MESSAGE);
+		} catch (UserNotFoundException e) {
+			Notification.show("Hiba a kiválasztott tanárral!", Notification.Type.ERROR_MESSAGE);
 		}
 	}
 
 	private Component createCourseEditForm(BeanFieldGroup<Course> courseBinder) {
 		FormLayout formLayout = new FormLayout();
 
-		TextField idField = courseBinder.buildAndBind("Előadás azonosítója:", "id", TextField.class);
+		idField = courseBinder.buildAndBind("Előadás azonosítója:", "id", TextField.class);
 		idField.setReadOnly(true);
 		idField.setWidth("100%");
 		formLayout.addComponent(idField);
 
-		TextField nameField = courseBinder.buildAndBind("Előadás címe:", "name", TextField.class);
+		nameField = courseBinder.buildAndBind("Előadás címe:", "name", TextField.class);
 		nameField.setNullRepresentation("");
 		nameField.setWidth("100%");
+		nameField.addValidator(createNotNullValidator("Az előadás címe nem lehet üres."));
 		formLayout.addComponent(nameField);
 
-		TextField creditField = courseBinder.buildAndBind("Kredit:", "credit", TextField.class);
+		creditField = courseBinder.buildAndBind("Kredit:", "credit", TextField.class);
 		creditField.setNullRepresentation("");
 		creditField.setWidth("100%");
+		creditField.addValidator(new IntegerRangeValidator(
+				"A kreditnek " + Global.MIN_CREDIT + " és " + Global.MAX_CREDIT + " Között kell lennie.",
+				Global.MIN_CREDIT, Global.MAX_CREDIT));
+		creditField.addValidator(createNotNullValidator("Kötelező értéket adni a kredithez."));
 		formLayout.addComponent(creditField);
 
-		TextField roomField = courseBinder.buildAndBind("Terem:", "room", TextField.class);
+		roomField = courseBinder.buildAndBind("Terem:", "room", TextField.class);
 		roomField.setNullRepresentation("");
 		roomField.setWidth("100%");
+		roomField.addValidator(createNotNullValidator("A terem mező nem lehet üres."));
 		formLayout.addComponent(roomField);
 
-		TextField sizeField = courseBinder.buildAndBind("Maximum férőhelyek:","maxSize",TextField.class);
+		sizeField = courseBinder.buildAndBind("Maximum férőhelyek:", "maxSize", TextField.class);
 		sizeField.setNullRepresentation("");
 		sizeField.setWidth("100%");
+		sizeField.addValidator(createNotNullValidator("A mérethez kell írni érétket."));
+		sizeField.addValidator(createNotNegativValidator("A maximum méret nem lehet negatív"));
 		formLayout.addComponent(sizeField);
-		
-		TextArea descriptionArea = courseBinder.buildAndBind("Leírás:","description", TextArea.class);
+
+		descriptionArea = courseBinder.buildAndBind("Leírás:", "description", TextArea.class);
 		descriptionArea.setNullRepresentation("");
 		descriptionArea.setWidth("100%");
 		descriptionArea.setHeight("150px");
 		formLayout.addComponent(descriptionArea);
-		
-		ComboBox teacherField = new ComboBox("Előadó:");
+
+		teacherField = new ComboBox("Előadó:");
 		teacherField.setNullSelectionAllowed(false);
 		Course bean = courseBinder.getItemDataSource().getBean();
 		for (RegisteredUser teacher : dao.listAllUsers()) {
@@ -177,6 +208,30 @@ public class AdminCourseListView extends AdminCourseListDesign implements View {
 		formLayout.setSpacing(true);
 
 		return formLayout;
+	}
+
+	private Validator createNotNegativValidator(final String message) {
+		return new Validator() {
+
+			@Override
+			public void validate(Object value) throws InvalidValueException {
+				if (value != null && Integer.parseInt(value.toString()) < 0) {
+					throw new InvalidValueException(message);
+				}
+			}
+		};
+	}
+
+	private Validator createNotNullValidator(final String message) {
+		return new Validator() {
+
+			@Override
+			public void validate(Object value) throws InvalidValueException {
+				if (value == null || "".compareTo(value.toString()) == 0) {
+					throw new InvalidValueException(message);
+				}
+			}
+		};
 	}
 
 	@Override
